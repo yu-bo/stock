4597acb6706cc3b2a1f383e80f28a2de4605de45
yu-bo <wizard-k@163.com>
2020-01-09 22:03:00 +0800

    2020-1-9

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import datetime
 import os
 import numpy as np
+import pandas as pd
 
 ts.set_token("d1af48f518c17415b1b98b2ce84ab7b1a0025adfdde78e22513b31ec")
 pro = ts.pro_api()
@@ -24,37 +25,53 @@ d={"ts_code":"TS代码",
 "is_hs":"是否沪深港通标的，N否 H沪股通 S深股通",
 }
 
-fields=list(d.keys())
-headers= list (d.values())
-data = pro.query('stock_basic', exchange='', list_status='L', fields=fields)
-#print(data.values)
-#data.to_csv("test.csv",header=True)
-
-with open("data/stock.csv","w",newline="") as f:
-    file_writer= csv.writer(f)
-    file_writer.writerow(headers)
-    file_writer.writerows(data.values)
 
 
-stock_list = [] 
-today=datetime.date.today().strftime("%Y%m%d")
-with open("data/data.csv","w",newline="") as f:
-    file_writer= csv.writer(f)
-    headers.append("更新日期")
-    file_writer.writerow(headers)
-    for row in data.values:
-        np.append(row,np.array([today]))
-        stock_list.append(row )
-    file_writer.writerows(stock_list)
-
-
-
-# with open("data/stock.csv","r") as f:
-#     file_reader = csv.reader(f)
+def stock_record():
+    # 获取信息
+    fields=list(d.keys())
+    headers= list (d.values())
+    data = pro.query('stock_basic', exchange='', list_status='L', fields=fields)
+    #print(data.values)
+    data.columns=headers
+    data.to_csv("data/stock.csv",header=True,encoding='utf-8',index=False)
     
-#     for i,row in enumerate (file_reader):
-#         row.append("采集日期")
-#         #stock_list.append()
-#         #print(row)
+
+    # with open("data/stock.csv","w",newline="") as f:
+    #     file_writer= csv.writer(f)
+    #     file_writer.writerow(headers)
+    #     file_writer.writerows(data.values)
+
+def stock_data():
+    #初始化跟新时间
+    data= pd.read_csv("data/stock.csv",dtype=object)
+    a= list(d.values())
+    b= ["TS代码","上市日期","退市日期"]
+    today =datetime.date.today().strftime("%Y%m%d")
+
+    header = [val for val in a if val not in b]
+    print(header)
+    data_1= data.drop(header,axis=1)
+    data_1.insert(loc=len(data_1.columns),column="跟新时间",value=today,allow_duplicates=False)
+    
+    for row in data_1.values:
+        row[3]=row[1]
+     
+    data_1.to_csv("data/data.csv",header=True,encoding='utf-8',index=False)
 
 
+def get_info():
+    data = pd.read_csv("data/data.csv", dtype=object)
+    today= datetime.date.today().strftime("%Y%m%d")
+    for row in data.values:
+        start_time =row[len(row)-1]
+        end_time =today
+        code = row[0]
+        df =pro.daily(ts_code=code, start_date=start_time, end_date=end_time)
+        print(df)
+
+
+if __name__ == "__main__":
+    #stock_record()
+    #stock_data()
+    get_info()
